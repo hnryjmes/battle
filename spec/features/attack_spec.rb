f5712eabd6ec8aa6b81473edebf00e86f095f902
Henry Cooksley <henry.cooksley@gmail.com>
2018-10-17 15:02:54 +0100

    finish section implementing hit points

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,11 @@ feature 'Attacking' do
     click_button 'Attack'
     expect(page).to have_content 'Alice attacked Bob'
   end
+  scenario 'reduce Player 2 hit points' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'OK'
+    expect(page).not_to have_content '60HP'
+    expect(page).to have_content '50HP'
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,9 +1,26 @@
 require 'player'
 describe Player do
-  subject(:player) {described_class.new("Bob")}
+  let(:bob) { described_class.new("Bob") }
+  let(:alice) { described_class.new("Alice") }
   describe '#name' do
     it 'returns name' do
-      expect(player.name).to eq ("Bob")
+      expect(bob.name).to eq ("Bob")
+    end
+  end
+  describe '#attack' do
+    it 'attacks player' do
+      expect(alice).to receive(:receive_damage)
+      bob.attack(alice)
+    end
+  end
+  describe '#hit' do
+    it 'reduces player hit points' do
+      expect{ alice.receive_damage }.to change{ alice.hit_points }.by(-10)
+    end
+  end
+  describe '#hit_points' do
+    it 'has a default value of 60' do
+      expect(bob.hit_points).to eq described_class::DEFAULT_HIT_POINTS
     end
   end
 end
